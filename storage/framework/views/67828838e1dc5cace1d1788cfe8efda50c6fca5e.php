74266d6440b2df9b853fe79897cd3df2c7777ee8
JavedYcet <70424511+ChJavedIqbal@users.noreply.github.com>
2023-03-26 19:25:21 +0530

    Add files via upload

---
+++ storage/framework/views/67828838e1dc5cace1d1788cfe8efda50c6fca5e.php
@@ -0,0 +1,5 @@
+<h1><?php echo e(__('profile.welcome')); ?></h1>
+<a href=""><?php echo e(__('profile.about')); ?></a>
+<a href=""><?php echo e(__('profile.list')); ?></a>
+<a href=""><?php echo e(__('profile.contact')); ?></a>
+<?php /**PATH /var/www/html/localization/resources/views/profile.blade.php ENDPATH**/ ?>
